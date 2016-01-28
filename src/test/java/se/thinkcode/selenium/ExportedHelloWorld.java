83cecd8879736cb3b86da536b35fa8e1c03e4fa9
Thomas Sundberg <tsu@kth.se>
2016-01-28 20:48:40 +0100

    A test case exported from Selenium IDE

---
+++ src/test/java/se/thinkcode/selenium/ExportedHelloWorld.java
@@ -0,0 +1,43 @@
+package se.thinkcode.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ExportedHelloWorld {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "http://selenium.thinkcode.se/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    @Test
+    public void testExportedHelloWorld() throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.id("helloWorld")).click();
+        assertEquals("Hello, world!", driver.findElement(By.id("headline")).getText());
+    }
+}
