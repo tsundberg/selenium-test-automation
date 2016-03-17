147481eccd0a23332e9a893a35ee197edc9cffca
Thomas Sundberg <tsu@kth.se>
2016-03-17 19:16:29 +0100

    Two examples of data driven tests

---
+++ src/test/java/se/thinkcode/selenium/datadriven/ChangePasswordForLoopTest.java
@@ -0,0 +1,56 @@
+package se.thinkcode.selenium.datadriven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import se.thinkcode.selenium.form.ConfirmPasswordSentPage;
+import se.thinkcode.selenium.form.RequestPasswordPage;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ChangePasswordForLoopTest {
+    private WebDriver browser;
+    private String baseUrl;
+
+    @Before
+    public void setUp() {
+        browser = new FirefoxDriver();
+        baseUrl = "http://selenium.thinkcode.se/requestPassword";
+    }
+
+    @After
+    public void tearDown() {
+        browser.quit();
+    }
+
+    @Test
+    public void request_new_password() {
+        for (String account : accounts()) {
+            String expected = "A new password has been sent to " + account;
+
+            browser.get(baseUrl);
+            RequestPasswordPage requestPasswordPage = new RequestPasswordPage(browser);
+            ConfirmPasswordSentPage confirmPasswordSentPage = requestPasswordPage.requestNewPassword(account);
+
+            String actual = confirmPasswordSentPage.getConfirmationMessage();
+
+            assertThat(actual, is(expected));
+        }
+    }
+
+    private Collection<String> accounts() {
+        Collection<String> accounts = new LinkedList<String>();
+
+        accounts.add("Sune");
+        accounts.add("Gretchen");
+
+        return accounts;
+    }
+
+}
---
+++ src/test/java/se/thinkcode/selenium/datadriven/ChangePasswordTest.java
@@ -0,0 +1,62 @@
+package se.thinkcode.selenium.datadriven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import se.thinkcode.selenium.form.ConfirmPasswordSentPage;
+import se.thinkcode.selenium.form.RequestPasswordPage;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(Parameterized.class)
+public class ChangePasswordTest {
+    private WebDriver browser;
+
+    private String account;
+
+    public ChangePasswordTest(String account) {
+        this.account = account;
+    }
+
+    @Before
+    public void setUp() {
+        browser = new FirefoxDriver();
+        String baseUrl = "http://selenium.thinkcode.se/requestPassword";
+        browser.get(baseUrl);
+    }
+
+    @After
+    public void tearDown() {
+        browser.quit();
+    }
+
+    @Test
+    public void request_new_password(){
+        String expected = "A new password has been sent to " + account;
+
+        RequestPasswordPage requestPasswordPage = new RequestPasswordPage(browser);
+        ConfirmPasswordSentPage confirmPasswordSentPage = requestPasswordPage.requestNewPassword(account);
+
+        String actual = confirmPasswordSentPage.getConfirmationMessage();
+
+        assertThat(actual, is(expected));
+    }
+
+    @Parameterized.Parameters
+    public static Collection<String[]> accounts() {
+        Collection<String[]> accounts = new LinkedList<String[]>();
+
+        accounts.add(new String[]{"Sune"});
+        accounts.add(new String[]{"Gretchen"});
+
+        return accounts;
+    }
+}
